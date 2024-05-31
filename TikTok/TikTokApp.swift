b4b66d078ca3ec076c05a4bf4c738f35e22b2331
Mohamed-Aldakeshy <hassounaaldakeshi88@gmail.com>
2024-05-31 18:43:12 +0200

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Mohamed Aldakeshy on 31/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
